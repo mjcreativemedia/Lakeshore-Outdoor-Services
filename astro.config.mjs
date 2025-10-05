35cbfdcfd10c15d9e7f6c19483cffbc310c017ee
Mitch Argamasilla <64268858+mjcreativemedia@users.noreply.github.com>
2025-10-05 11:12:46 -0500

    chore: remove @astrojs/robots; add lockfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import robots from "@astrojs/robots";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: "https://lakeshoreoutdoor.com",
-  output: "static",
+  site: 'https://lakeshoreoutdoor.com',
+  output: 'static',
   integrations: [
-    sitemap(),
-    robots({
-      host: "https://lakeshoreoutdoor.com",
-      sitemap: "https://lakeshoreoutdoor.com/sitemap-index.xml",
-      policy: [{ userAgent: "*", allow: "/" }]
-    })
+    sitemap({ entryLimit: 45000 })
   ]
 });
